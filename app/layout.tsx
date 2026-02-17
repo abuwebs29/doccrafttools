3defd3b656aa0e0c361e1f0d58ed346b27d272e4
abuwebs29 <huraira.abu@aol.com>
2026-02-17 15:24:56 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-PASTE-YOUR-ID-HERE');
+            gtag('config', 'G-SKR2R64LCM');
           `}
         </Script>
       </head>
