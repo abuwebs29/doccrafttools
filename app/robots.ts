a753416c876ad04ad14a0120c0415b44df7be0c7
abuwebs29 <huraira.abu@aol.com>
2026-03-17 11:23:58 +0400

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,36 @@
 import type { MetadataRoute } from "next";
 
+const BASE_URL = "https://doccrafttools.com";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://doccrafttools.com/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+
+        allow: [
+          "/",
+          "/invoice-generator",
+          "/receipt-generator",
+          "/quotation-generator",
+          "/delivery-note-generator",
+          "/rent-receipt-generator",
+          "/pdf-templates",
+        ],
+
+        disallow: [
+          "/search",
+          "/status",
+          "/api/",
+          "/_next/",
+          "/embed/",
+          "/invoice-generator-",
+          "/receipt-generator-",
+        ],
+      },
+    ],
+
+    sitemap: `${BASE_URL}/sitemap.xml`,
+    host: BASE_URL,
   };
 }
